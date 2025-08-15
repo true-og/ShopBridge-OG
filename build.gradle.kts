e103157e0d686fe692ff335bfe98de48cc03e5b8
NotAlexNoyle <alexnoyle@icloud.com>
2025-08-15 16:12:59 -0400

    Update gradle to 8.14.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,28 @@
+/* This is free and unencumbered software released into the public domain */
 import java.io.BufferedReader
 
+/* ------------------------------ Plugins ------------------------------ */
 plugins {
-    kotlin("jvm") version "2.1.21" // Import kotlin jvm plugin for kotlin/java integration.
-    id("com.diffplug.spotless") version "7.0.4" // Import auto-formatter.
-    id("com.gradleup.shadow") version "8.3.6" // Import shadow API.
-    eclipse // Import eclipse plugin for IDE integration.
+    id("java") // Import Java plugin.
+    id("java-library") // Import Java Library plugin.
+    id("com.diffplug.spotless") version "7.0.4" // Import Spotless plugin.
+    id("com.gradleup.shadow") version "8.3.6" // Import Shadow plugin.
+    eclipse // Import Eclipse plugin.
+    kotlin("jvm") version "2.1.21" // Import Kotlin JVM plugin.
 }
 
+/* --------------------------- JDK / Kotlin ---------------------------- */
+java {
+    sourceCompatibility = JavaVersion.VERSION_17 // Compile with JDK 17 compatibility.
+    toolchain { // Select Java toolchain.
+        languageVersion.set(JavaLanguageVersion.of(17)) // Use JDK 17.
+        vendor.set(JvmVendorSpec.GRAAL_VM) // Use GraalVM CE.
+    }
+}
+
+kotlin { jvmToolchain(17) }
+
+/* ----------------------------- Metadata ------------------------------ */
 val commitHash =
     Runtime.getRuntime().exec(arrayOf("git", "rev-parse", "--short=10", "HEAD")).let { process ->
         process.waitFor()
@@ -21,76 +37,64 @@ version = "1.1.6" // Declare plugin version (will be in .jar).
 
 val apiVersion = "1.19" // Declare minecraft server target version.
 
+/* ----------------------------- Resources ----------------------------- */
 tasks.named<ProcessResources>("processResources") {
     val props = mapOf("version" to version, "apiVersion" to apiVersion)
-
     inputs.properties(props) // Indicates to rerun if version changes.
-
     filesMatching("plugin.yml") { expand(props) }
-    from("LICENSE") { // Bundle license into .jars.
-        into("/")
-    }
+    from("LICENSE") { into("/") } // Bundle licenses into jarfiles.
 }
 
+/* ---------------------------- Repos ---------------------------------- */
 repositories {
-    mavenCentral()
-    gradlePluginPortal()
-    maven { url = uri("https://repo.purpurmc.org/snapshots") }
-    maven { url = uri("https://repo.purpurmc.org/snapshots") }
-    maven { url = uri("https://repo.codemc.io/repository/maven-public/") }
-    maven { url = uri("https://maven.enginehub.org/repo/") }
+    mavenCentral() // Import the Maven Central Maven Repository.
+    gradlePluginPortal() // Import the Gradle Plugin Portal Maven Repository.
+    maven { url = uri("https://repo.purpurmc.org/snapshots") } // Import the PurpurMC Maven Repository.
+    maven { url = uri("https://repo.codemc.io/repository/maven-public/") } // Import the CodeMC Maven Repository.
+    maven { url = uri("https://maven.enginehub.org/repo/") } // Import the EngineHub Maven Repository.
 }
 
+/* ---------------------- Java project deps ---------------------------- */
 dependencies {
-    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Declare purpur API version to be packaged.
+    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Declare Purpur API version to be packaged.
     compileOnly("com.ghostchu:quickshop-api:5.2.0.7") // Import QuickShop-Hikari Core API.
     implementation("com.ghostchu.quickshop.compatibility:common:5.2.0.7") // Import QuickShop-Hikari Kotlin Helper API.
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.8") // Import WorldGuard API.
     compileOnly(files("libs/AreaShop-2.9.1.jar")) // Import AreaShop API.
 }
 
+/* ---------------------- Reproducible jars ---------------------------- */
 tasks.withType<AbstractArchiveTask>().configureEach { // Ensure reproducible .jars
     isPreserveFileTimestamps = false
     isReproducibleFileOrder = true
 }
 
-val targetJavaVersion = 17
-
-kotlin { jvmToolchain(targetJavaVersion) }
-
+/* ----------------------------- Shadow -------------------------------- */
 tasks.shadowJar {
     exclude("io.github.miniplaceholders.*") // Exclude the MiniPlaceholders package from being shadowed.
     archiveClassifier.set("") // Use empty string instead of null.
     minimize()
 }
 
-tasks.build {
-    dependsOn(tasks.spotlessApply)
-    dependsOn(tasks.shadowJar)
-}
+tasks.jar { archiveClassifier.set("part") } // Applies to root jarfile only.
 
-tasks.jar { archiveClassifier.set("part") }
+tasks.build { dependsOn(tasks.spotlessApply, tasks.shadowJar) } // Build depends on spotless and shadow.
 
+/* --------------------------- Javac opts ------------------------------- */
 tasks.withType<JavaCompile>().configureEach {
-    options.compilerArgs.add("-parameters")
-    options.compilerArgs.add("-Xlint:deprecation") // Triggers deprecation warning messages.
-    options.encoding = "UTF-8"
-    options.isFork = true
+    options.compilerArgs.add("-Xlint:deprecation") // Trigger deprecation warning messages.
+    options.encoding = "UTF-8" // Use UTF-8 file encoding.
 }
 
-kotlin { jvmToolchain(17) }
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-        vendor = JvmVendorSpec.GRAAL_VM
-    }
-}
-
+/* ----------------------------- Auto Formatting ------------------------ */
 spotless {
     kotlin { ktfmt().kotlinlangStyle().configure { it.setMaxWidth(120) } }
     kotlinGradle {
-        ktfmt().kotlinlangStyle().configure { it.setMaxWidth(120) }
-        target("build.gradle.kts", "settings.gradle.kts")
+        ktfmt().kotlinlangStyle().configure { it.setMaxWidth(120) } // JetBrains Kotlin formatting.
+        target("build.gradle.kts", "settings.gradle.kts") // Gradle files to format.
     }
+}
+
+tasks.named("spotlessCheck") {
+    dependsOn("spotlessApply") // Run spotless before checking if spotless ran.
 }

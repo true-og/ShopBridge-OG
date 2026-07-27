5ef2fc37bb274ee96ca0543bccd6a4262c82dc7b
NotAlexNoyle <alexnoyle@icloud.com>
2026-07-27 09:21:31 -0400

    Move to AreaShop-OG

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     compileOnly("com.ghostchu:quickshop-api:5.2.0.7") // Import QuickShop-Hikari Core API.
     implementation("com.ghostchu.quickshop.compatibility:common:5.2.0.7") // Import QuickShop-Hikari Kotlin Helper API.
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.8") // Import WorldGuard API.
-    compileOnly(files("libs/AreaShop-2.9.1.jar")) // Import AreaShop API.
+    compileOnly(files("libs/AreaShop-2.9.1.jar")) // Import AreaShop-OG compatible upstream API.
 }
 
 /* ---------------------- Reproducible jars ---------------------------- */
